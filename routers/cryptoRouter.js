7f0ccb5e469531b9178bf30a32b308c08debc55b
Ridho Bagaskara <ridhobagaskara68@gmail.com>
2022-01-15 23:47:18 +0700

    backend 2

--- routers/cryptoRouter.js
+++ routers/cryptoRouter.js
@@ -3,6 +3,9 @@ const { CryptoController } = require('../controllers/cryptoController');
 
 var router = express.Router();
 
+router.get('/', function(res,req) {
+    res.json('hallo');
+});
 router.post('/', CryptoController.send);
 
 module.exports = router;
